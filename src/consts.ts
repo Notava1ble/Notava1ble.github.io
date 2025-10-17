16eeced1c4f5d9aae657ac6bdbce7f05d6251429
Visar <visarmullaj07@gmail.com>
2025-10-17 23:25:11 +0200

    Fixes + removed contact form

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Visar Mullaj";
-export const SITE_DESCRIPTION = "A personal website for Visar Mullaj!";
+export const SITE_DESCRIPTION =
+  "Portfolio of Visar Mullaj, a frontend developer building fast and usable web apps";
